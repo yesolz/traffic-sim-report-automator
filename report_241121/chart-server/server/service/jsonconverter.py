1ace44b26262750a701cbb7835a91b98bb76003e
yesolz <yesol@ajou.ac.kr>
2025-03-21 22:42:52 +0900

    fix: prevent JSON serialization error by using default=str

--- report_241121/chart-server/server/service/jsonconverter.py
+++ report_241121/chart-server/server/service/jsonconverter.py
@@ -777,7 +777,7 @@ class json_converter():
         }
     
         with open(output_path, "w", encoding="utf-8") as outfile:
-            json.dump(data, outfile, indent=4, ensure_ascii=False)
+            json.dump(data, outfile, indent=4, ensure_ascii=False,  default=str)
     
         print(f"Data successfully saved to {output_path}")
 
